84b0fa155ced7fd1c9f007bde52a6a4911e96953
Luana Stephany <ls.luanastephany@gmail.com>
2022-10-19 11:13:41 -0700

    setting card type

--- src/main.js
+++ src/main.js
@@ -1,20 +1,19 @@
-import "./css/index.css"
+import "./css/index.css";
 
-const ccBackground1 = document.querySelector(
-  ".cc-bg svg > g g:nth-child(1) path"
-)
-const ccBackground2 = document.querySelector(
-  ".cc-bg svg > g g:nth-child(2) path"
-)
+const ccBackground1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
+const ccBackground2 = document.querySelector(".cc-bg svg > g g:nth-child(2) path");
+const ccLogo = document.querySelector(".cc-logo span:nth-child(2) img");
 
 function setCardType(type) {
   const colors = {
     visa: ["#436D99", "#2D57F2"],
     mastercard: ["#DF6F29", "#C69347"],
     default: ["black", "grey"],
-  }
-  ccBackground1.setAttribute("fill", colors[type][0])
-  ccBackground2.setAttribute("fill", colors[type][1])
+  };
+  ccBackground1.setAttribute("fill", colors[type][0]);
+  ccBackground2.setAttribute("fill", colors[type][1]);
+  ccLogo.setAttribute("src", `cc-${type}.svg`);
 }
 
-setCardType("visa")
+//setCardType("mastercard");
+globalThis.setCardType = setCardType;
