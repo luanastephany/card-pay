11597fab89470f0fd4f8d6fe898694047707e103
Luana Stephany <ls.luanastephany@gmail.com>
2022-10-19 16:14:40 -0700

    adding regex and IMask

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import IMask from "imask";
 import "./css/index.css";
 
 const ccBackground1 = document.querySelector(".cc-bg svg > g g:nth-child(1) path");
@@ -17,3 +18,59 @@ function setCardType(type) {
 
 //setCardType("mastercard");
 globalThis.setCardType = setCardType;
+
+//CVC
+const securityCode = document.querySelector("#security-code");
+const securityCodePattern = {
+  mask: "000",
+};
+const securityCodeMasked = IMask(securityCode, securityCodePattern);
+
+//expiry date
+const expirationDate = document.querySelector("#expiration-date");
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+  },
+};
+const expirationDateMasked = IMask(expirationDate, expirationDatePattern);
+
+//Card number
+const cardNumber = document.querySelector("#card-number");
+const cardNumberPattern = {
+  mask: [
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^4\d{0,15}/,
+      cardType: "visa",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      regex: /^(5[1-5]\d{0,2}|22[2-9]\d{0,1}|2[3-7]\d{0,2})\d{0,12}/,
+      cardType: "mastercard",
+    },
+    {
+      mask: "0000 0000 0000 0000",
+      cardType: "default",
+    },
+  ],
+  //This function will be triggered every time a number is entered on card number input
+  dispatch: function (appended, dynamicMasked) {
+    const number = (dynamicMasked.value + appended).replace(/\D/g, "");
+    const foundMask = dynamicMasked.compiledMasks.find(function (item) {
+      return number.match(item.regex);
+    });
+    return foundMask;
+  },
+};
+const cardNumberMasked = IMask(cardNumber, cardNumberPattern);
